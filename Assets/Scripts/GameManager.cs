86bfd28fc40e639e22e7fb2ad894fbfd223cec54
Ian <ianthomson92@gmail.com>
2025-03-08 22:17:19 +0000

    Spline

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,7 +5,10 @@ using Events;
 using Gameplay;
 using Gameplay.TrackEvents;
 using SGS29.Utilities;
+using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Splines;
 
 // [ExecuteAlways]
 public class GameManager : MonoBehaviour
@@ -14,6 +17,8 @@ public class GameManager : MonoBehaviour
     [SerializeField] private TrackPlayer _trackPlayer;
     [SerializeField] private LevelGenerator _levelGenerator;
     [SerializeField] private Transform _shipPrefab;
+    [SerializeField] private Spline _spline;
+    public SplineContainer _container;
     private EventManager _eventManager;
     private Dictionary<Gameplay.Beat, float> _normalizedBeatTimes;
     private TrackDefinition _trackDefinition;
@@ -22,6 +27,7 @@ public class GameManager : MonoBehaviour
     private float _progress;
     private Level _currentLevel;
     private Transform _playerShip;
+    private SplineAnimate _splineAnimate;
 
     enum GameplayState
     {
@@ -41,6 +47,11 @@ public class GameManager : MonoBehaviour
         _eventManager.RegisterListener<GameOver>(evt => TrackFailed());
         _eventManager.RegisterListener<TrackPassed>(evt => TrackPassed());
         _eventManager.RegisterListener<GameOver>(evt => Debug.Log("GAME OVER LOSER"));
+
+        _container = FindAnyObjectByType<SplineContainer>();
+        
+        _splineAnimate = _playerShip.GetComponent<SplineAnimate>();
+
     }
 
     public void Start()
@@ -59,10 +70,22 @@ public class GameManager : MonoBehaviour
 
     private void Update()
     {
+        
+        if (_currentState == GameplayState.Transitioning)
+        {
+            if (!_splineAnimate.IsPlaying)
+            {
+
+                _currentState = GameplayState.OnTrack;
+            }
+        }
+        
         if (_currentState != GameplayState.OnTrack)
         {
             return;
         }
+
+ 
 
         _progress += Time.deltaTime;
         _trackPlayer.Tick(_progress);
@@ -95,6 +118,24 @@ public class GameManager : MonoBehaviour
 
     private void TrackPassed()
     {
+        var pos1 = OrbitHelpers.OrbitPointFromNormalisedPosition(_currentLevel.World.Orbit, 0.75f);
+        var pos3 = OrbitHelpers.OrbitPointFromNormalisedPosition(_levels[1].World.Orbit, 0.5f);
+        var pos2 = new Vector3((pos1.x + pos3.x) / 2, 0, -2500f);
+
+        var knot1 = new BezierKnot(pos1, pos1.x, pos1.x + 1000);
+        var knot3 = new BezierKnot(pos3, pos3.z, pos3.z + 750);
+        _spline.Add(knot1, TangentMode.Mirrored);
+        _spline.Add(pos2);
+        _spline.Add(knot3, TangentMode.Mirrored);
+        
+
+        _container.Spline = _spline;
+        _splineAnimate.Container = _container;
+        
+        _splineAnimate.Duration = 10f;
+        _splineAnimate.Loop = SplineAnimate.LoopMode.Once;
+        _splineAnimate.Play();
+        
         _currentState = GameplayState.Transitioning;
     }
 
