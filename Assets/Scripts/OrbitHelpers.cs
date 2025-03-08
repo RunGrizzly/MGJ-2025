75d1fd3b11b82d223162f5fd84715b1f691eb5fe
Josh Desktop | P400A <joshua.henderson006@gmail.com>
2025-03-08 16:49:53 +0000

    Added OrbitHelpers.cs

---
+++ Assets/Scripts/OrbitHelpers.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class Orbit
+{
+    public Transform Centre;
+    public Vector3 Offset;
+    public float Radius;
+    public Vector3 Axis;
+
+    public Color Color = UnityEngine.Color.white;
+
+    public Orbit(Transform centre ,Vector3 offset, float radius, Vector3 axis)
+    {
+        Centre = centre;
+        Offset = offset;
+        Radius = radius;
+        Axis = axis;
+    }
+}
+
+
+public static class OrbitHelpers
+{
+    public static Vector3 OrbitPointFromNormalisedPosition(Orbit orbit, float normalisedPosition)
+    {
+        var angle = -Mathf.Deg2Rad * (normalisedPosition * 360);
+
+        return orbit.Centre.position + orbit.Offset + orbit.Centre.localScale.x * orbit.Radius * ((Mathf.Cos(angle) * orbit.Centre.transform.right) + (Mathf.Sin(angle) * orbit.Centre.transform.forward));
+    }
+
+    public static Quaternion ForwardRotationFromNormalisePosition(Orbit orbit, float normalisedPosition)
+    {
+        var angle = -Mathf.Deg2Rad * (normalisedPosition * 360);
+
+        return Quaternion.LookRotation((Mathf.Sin(angle) * orbit.Centre.transform.right) - (Mathf.Cos(angle) * orbit.Centre.transform.forward), orbit.Centre.transform.up);
+    }
+}
